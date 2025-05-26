544962a711956770726e779098d1a7383b2c6950
HOZY39 <igor05061@gmail.com>
2025-05-26 23:18:18 +0200

    init

---
+++ src/main/kotlin/com/simon/dragoneclipse/DragoneclipseApplication.kt
@@ -0,0 +1,11 @@
+package com.simon.dragoneclipse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DragoneclipseApplication
+
+fun main(args: Array<String>) {
+	runApplication<DragoneclipseApplication>(*args)
+}
---
+++ src/test/kotlin/com/simon/dragoneclipse/DragoneclipseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.simon.dragoneclipse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DragoneclipseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
